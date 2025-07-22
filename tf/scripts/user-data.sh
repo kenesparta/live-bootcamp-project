71056ad52fabbab44f2ae9578ff523ded414f5db
Ken Esparta <kenesparta@pm.me>
2025-07-22 13:05:55 -0500

    fix: Update ec2 script

--- tf/scripts/user-data.sh
+++ tf/scripts/user-data.sh
@@ -17,13 +17,15 @@ function install_docker() {
 }
 
 function install_docker_compose() {
-  DOCKER_CONFIG="${DOCKER_CONFIG:-$HOME/.docker}"
+  sudo su ec2-user
+  DOCKER_CONFIG="${DOCKER_CONFIG:-/home/ec2-user/.docker}"
   mkdir -p "${DOCKER_CONFIG}/cli-plugins"
   curl -SL https://github.com/docker/compose/releases/download/v2.38.2/docker-compose-linux-x86_64 -o "${DOCKER_CONFIG}/cli-plugins/docker-compose"
   chmod +x "${DOCKER_CONFIG}/cli-plugins/docker-compose"
 }
 
 function install_nginx() {
+  sudo su
   sudo yum install -y nginx
   sudo systemctl start nginx
   sudo systemctl enable nginx
